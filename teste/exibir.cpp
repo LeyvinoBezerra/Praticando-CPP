f8decfed12f209d59cbb8616ec949717dce7626a
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2024-05-01 14:25:39 -0300

    teste

---
+++ teste/exibir.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+//void exibir();
+
+int main() {
+	
+	int cont = 1;
+
+	for (int i = 0; i < 1000000; ++i)
+		// exibir();
+	    cout << cont++ << endl;
+
+
+
+
+	return 0;
+}
+
+//void exibir()
+//{
+	//static int cont = 1;
+	//cout << cont++ << endl;
+//}
